ee1a40f50582c7a8f70e554a9763282893bc174b
ugur <yuciyuar@users.noreply.github.com>
2015-06-23 18:23:21 +0300

    route links shortened
    portfolio/index/parameterName links changed for clean view

--- application/views/blocks/navbar.php
+++ application/views/blocks/navbar.php
@@ -3,9 +3,9 @@
         <li><?php echo (isset($dashLink) ? '<li><a href="'.$dashLink.'">dashboard</a></li>' : null); ?></li>
     </ul>
     <ul class="pull-right list-inline">
-        <li><a href="<?php echo base_url('portfolio/index/about')?>">about</a></li>
-        <li><a href="<?php echo base_url('portfolio/index/projects')?>">projects</a></li>
-        <li><a href="<?php echo base_url('portfolio/index/contact')?>">contact</a></li>
+        <li><a href="<?php echo base_url('about')?>">about</a></li>
+        <li><a href="<?php echo base_url('projects') //controlling portfolio/index/projects actually. just shortened for clean view ?>">projects</a></li>
+        <li><a href="<?php echo base_url('contact')?>">contact</a></li>
 
     </ul>
 </div>
